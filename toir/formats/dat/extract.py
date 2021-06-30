70eaaf018860765e7bfec83a42f2abf2cebe24ec
LostTemplarRH <78380415+LostTemplarRH@users.noreply.github.com>
2021-06-30 20:16:35 +0200

    Extract battle book and chara ability

--- toir/formats/dat/extract.py
+++ toir/formats/dat/extract.py
@@ -1,9 +1,13 @@
 from .artes import extract_artes
 from .items import extract_items
+from .battlebook import extract_battle_book
+from .charaability import extract_chara_ability
 
 _EXTRACTORS = [
     extract_items,
     extract_artes,
+    extract_battle_book,
+    extract_chara_ability,
 ]
 
 def extract_dat(l7cdir, outputdir):
--- toir/text.py
+++ toir/text.py
@@ -1,39 +1,45 @@
 import re
 
 def decode_control_code(text, i):
-    code = ord(text[i + 1])
-    if code == 0x01:
-        arg = ord(text[i + 3])
-        if arg == 0x03:
-            return '{emph}', i + 4
-        elif arg == 0x04:
-            return '{regular}', i + 4
+    try:
+        code = text[i + 1]
+        if code == 0x01:
+            arg = text[i + 3]
+            if arg == 0x03:
+                return '{emph}', i + 4
+            elif arg == 0x04:
+                return '{regular}', i + 4
+            else:
+                return '{01}', i + 2        
+        elif code == 0x02:
+            index = text[i + 3] + (text[i + 4] << 8)
+            return f'{{item:{index:04X}}}', i + 5
+        elif code == 0x03:
+            index = text[i + 3] + (text[i + 4] << 8)
+            return f'{{unknown03:{index:04X}}}', i + 5
+        elif code == 0x04:
+            index = text[i + 3] + (text[i + 4] << 8)
+            return f'{{number:{index:04X}}}', i + 5
+        elif code == 0x05:
+            arg = text[i + 3]
+            if arg == 0x01:
+                return '{variable}', i + 4
+            elif arg == 0x02:
+                return '{fixed}', i + 4
+            else:
+                return '{05}', i + 2
+        elif code == 0x40:
+            index = text[i + 3] + ((text[i + 4]) << 8)
+            return f'{{button:{index:04X}}}', i + 5
+        elif code == 0x41:
+            index = text[i + 3]
+            return f'{{unknown41:{index:02X}}}', i + 4
+        elif code == 0x42:
+            return '{triverse}', i + 2
         else:
-            return '{01}', i + 2        
-    elif code == 0x02:
-        index = ord(text[i + 3]) + (ord(text[i + 4]) << 8)
-        return f'{{item:{index:04X}}}', i + 5
-    elif code == 0x03:
-        index = ord(text[i + 3]) + (ord(text[i + 4]) << 8)
-        return f'{{unknown03:{index:04X}}}', i + 5
-    elif code == 0x04:
-        index = ord(text[i + 3]) + (ord(text[i + 4]) << 8)
-        return f'{{number:{index:04X}}}', i + 5
-    elif code == 0x05:
-        arg = ord(text[i + 3])
-        if arg == 0x01:
-            return '{variable}', i + 4
-        elif arg == 0x02:
-            return '{fixed}', i + 4
-        else:
-            return '{05}', i + 2
-    elif code == 0x40:
-        index = ord(text[i + 3]) + (ord(text[i + 4]) << 8)
-        return f'{{button:{index:04X}}}', i + 5
-    elif code == 0x42:
-        return '{unknown42}', i + 2
-    else:
-        return f'{{{code:02X}}}', i + 2
+            return f'{{{code:02X}}}', i + 2
+    except Exception as e:
+        raise ValueError(str(e) + f' ("{text}")')
 
 _PUNCTUATION = r'…\u3000、？！!《》○―＝\n♪【】「｢｣」』）～〜・々)'
 _REDUNDANT_FIXED = re.compile(f'{{fixed}}(?P<chars>[{_PUNCTUATION}]+)({{variable}}|$)')
@@ -49,29 +55,34 @@ def remove_redundant_cc(text):
     removed = re.sub(_REDUNDANT_FIXED, _remove_spacing_cc, text)
     return re.sub(_REDUNDANT_VARIABLE, _remove_variable_cc, removed)
 
-def _decode_text(buffer):
-    text = buffer.decode('utf-8')
-    text_cc = ''
-    last = 0
-    i = text.find('@')
-    while i != -1:
-        text_cc += text[last:i]
-        if i != -1:
-            cc, i = decode_control_code(text, i)
-            text_cc += cc
-            last = i
-        i = text.find('@', i)
-    text_cc += text[last:]
-    return text_cc.replace('\r', '')
+def get_next_end(buffer, offset, end):
+    i = buffer.find(b'\x00', offset)
+    if i == -1:
+        return end
+    else:
+        return min(i, end)
 
-def decode_text(buffer, offset):
-    for i in range(offset, len(buffer)):
-        if buffer[i] == 0:
-            subbuffer = buffer[offset:i]
-            return _decode_text(subbuffer)
+def decode_text(buffer, offset, max_len=0):
+    if not max_len:
+        end = len(buffer)
+    else:
+        end = offset + max_len
+
+    next_at = buffer.find(b'@', offset)
+    next_end = get_next_end(buffer, offset, end)
+    last = offset
+    text = ''
+    while next_at != -1 and next_at < next_end:
+        text += buffer[last:next_at].decode('utf-8')
+        cc, last = decode_control_code(buffer, next_at)
+        text += cc
+        next_at = buffer.find(b'@', last)
+        next_end = get_next_end(buffer, last, end)
+    text += buffer[last:next_end].decode('utf-8')
+    return text.replace('\r', '')
 
 def decode_text_fixed(buffer, offset, length):
-    return _decode_text(buffer[offset:offset+length])
+    return decode_text(buffer, offset, length)
 
 def encode_text(string):
     return string.encode('utf-8') + b'\0'
